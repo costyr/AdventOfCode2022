804c934619dc20757f3d18ca2c6437f706190674
Costi Rada <costi.rada@caphyon.com>
2022-12-05 11:54:32 +0200

    Day 5 solution

--- Day5.js
+++ Day5.js
@@ -1,24 +1,47 @@
-const { off } = require('process');
 const util = require('./Util.js');
 
 function isLetter(str) {
   return str.length === 1 && str.match(/[a-z]/i);
 }
 
+function Transform(aStacks, aMoves) {
+  for (let i = 0; i < aMoves.length; i++)
+  {
+    for (let j = 0; j < aMoves[i][0]; j++)
+      aStacks[aMoves[i][2] - 1].push(aStacks[aMoves[i][1] - 1].pop());
+  }
+  
+  let tops = [];
+  for (let i = 0; i < aStacks.length; i++)
+    tops.push(aStacks[i][aStacks[i].length - 1]);
+    
+  return tops.join('');
+}
+
+function Transform2(aStacks, aMoves) {
+  for (let i = 0; i < aMoves.length; i++)
+  {
+    let temp = [];
+    for (let j = 0; j < aMoves[i][0]; j++)
+      temp.push(aStacks[aMoves[i][1] - 1].pop());
+
+    for (let k = temp.length - 1; k >= 0; k--) 
+      aStacks[aMoves[i][2] - 1].push(temp[k]);
+  }
+  
+  let tops = [];
+  for (let i = 0; i < aStacks.length; i++)
+    tops.push(aStacks[i][aStacks[i].length - 1]);
+    
+  return tops.join('');
+}
+
 let state = 0;
 
-let stacks= [['1'],
-             ['2'],
-             ['3'],
-             ['4'],
-             ['5'],
-             ['6'],
-             ['7'],
-             ['8'],
-             ['9']];
+let stacks= [];
 let moves = [];
 
-let gg = util.MapInput('./Day5Input.txt', (aElem) => {
+util.MapInput('./Day5Input.txt', (aElem) => {
 
   if (state == 0) {
     if (aElem.startsWith(" 1"))
@@ -27,30 +50,39 @@ let gg = util.MapInput('./Day5Input.txt', (aElem) => {
     }
     else
     {
-      let offset = 0;
       for (let i = 0; i < aElem.length; i++)
        if (isLetter(aElem[i]))
        {
-         if ( i > 4)
+         if ( i > 1)
          {
-           offset += 3;
-           console.log(aElem[i] + " " + stacks[i - offset] + " " + (i - offset) + " " + offset);
-
-           stacks[i - offset].push(aElem[i]);  
+           let index = (i - 1) / 4;
+           for (let j = stacks.length - 1; j < index; j++ )
+             stacks.push([]);     
+           stacks[index].splice(0, 0, aElem[i]);  
          }
          else
          {
-           stacks[0].push(aElem[i]);
-           offset += 1;
+           if (stacks.length == 0)
+             stacks.push([]);
+           stacks[0].splice(0, 0, aElem[i]);
          }  
        }         
     }    
   }
   else {
-    
+    if (aElem.length > 0)
+      moves.push(aElem.replace("move ", "").replace(" from ", ",").replace(" to ", ",").split(",").map((yy)=>{
+        return parseInt(yy);
+      }));
   }
 
   return aElem;
   }, '\r\n');
 
 console.log(stacks);
+console.log(moves);
+
+let stacks0 = util.CopyObject(stacks);
+
+console.log(Transform(stacks, moves));
+console.log(Transform2(stacks0, moves));
