92e652e89cd71502a9adaf0136551c56057240ce
Costi Rada <costi.rada@caphyon.com>
2022-12-13 20:29:41 +0200

    Some work on day 9 part 2

--- Day9.js
+++ Day9.js
@@ -1,4 +1,5 @@
 const util = require('./Util.js');
+const mm = require('./Matrix.js');
 
 function ComputeKey(aPos) {
   return aPos.x + "#" + aPos.y;
@@ -9,22 +10,80 @@ function IsNear(aPos1, aPos2) {
   return ((aPos1.x == aPos2.x) || (aPos1.y == aPos2.y))  ? (dist <= 1) : (dist <= 2);
 }
 
-function UpdateKnotPos(aKnot, aNewPos, aPath) {
-  aKnot.x = aNewPos.x;
-  aKnot.y = aNewPos.y;
+function UpdateKnotPos(aKnot, aIsTail, aNewPos, aDir, aPath) {
+  
+  let oo = (aKnot.x != aNewPos.x) && (aKnot.y != aNewPos.y);
+  UpdateKnot(aKnot, aDir, oo, aNewPos);
 
-  let key = ComputeKey(aKnot);
-  if (aPath[key] == undefined)
-    aPath[key] = 1;
-  else
-    aPath[key]++;
+  if (aIsTail) {
+    let key = ComputeKey(aKnot);
+    if (aPath[key] == undefined)
+      aPath[key] = 1;
+    else
+      aPath[key]++;
+  }
+}
+
+function CopyNode(aNode1, aNode2) {
+  aNode1.x = aNode2.x;
+  aNode1.y = aNode2.y;
+}
+
+function UpdateKnot(aKnot, aDir, aMoveDiag, aHead) {
+  if (aDir == 'R') {
+    aKnot.x++;
+    if (aMoveDiag)
+      (aKnot.y < aHead.y) ? aKnot.y-- : aKnot.y++;
+  }
+  else if (aDir == 'L') {
+    aKnot.x--;
+    if (aMoveDiag)
+      (aKnot.y < aHead.y) ? aKnot.y++ : aKnot.y--;
+  }
+  else if (aDir == 'U') {
+    aKnot.y++;
+    if (aMoveDiag)
+      (aKnot.x < aHead.x) ? aKnot.x++ : aKnot.x--;
+  }
+  else {
+    aKnot.y--;
+    if (aMoveDiag)
+      (aKnot.x < aHead.x) ? aKnot.x-- : aKnot.x++;
+  }
+}
+
+function MoveKnots(aKnots, aDir, aCount, aTailPath) {
+  for (let j = 0; j < aCount; j++) {
+
+    if (aDir == 'L' && aCount == 8)
+      PrintKnots(aKnots);
+
+    UpdateKnot(aKnots[0], aDir, false, null);
+
+    for (let k = 1; k < aKnots.length; k++) {
+      if (!IsNear(aKnots[k], aKnots[k - 1])) {
+        UpdateKnotPos(aKnots[k], (k == (aKnots.length - 1)), aKnots[k - 1], aDir, aTailPath);
+      }
+    }
+  }  
+}
+
+function PrintKnots(aKnots) {
+   let grid = new mm.Matrix(30, 30, '.');
+   
+   let x = 10;
+   let y = 10;
+   for (let i = 0; i < aKnots.length; i++)
+   {
+     grid.SetValue(aKnots[i].y + y, aKnots[i].x + x, i == 0 ? 'H' : i.toString());
+   }
+
+   grid.PrintReverse('');
 }
 
 function ComputePath(aMoves, aKnotCount) {
 
   let tailPath = [];
-  let headPos = { x: 0, y: 0 };
-  let tailPos = { x: 0, y: 0 };
 
   let knots = [];
   for (let i = 0; i < aKnotCount; i++)
@@ -32,65 +91,22 @@ function ComputePath(aMoves, aKnotCount) {
 
   tailPath["0#0"] = 1;
   for (let i = 0; i < aMoves.length; i++) {
-    if (aMoves[i].dir == 'R') {
-      for (let j = 0; j < aMoves[i].count; j++) {
-        let prevHead = {x: headPos.x, y: headPos.y };
-        headPos.x++;
+    console.log("\n == " + aMoves[i].dir + " " + aMoves[i].count + " == \n");
 
-        for (let k = 1; k < knots.length; k++) {
-
-          let prev = (k == 1) ? prevHead : knots[k - 1];
-
-          if (!IsNear(headPos, tailPos)) 
-            UpdateKnotPos(tailPos, prevHead, tailPath);
-        }
-    }
-    }
-    else if (aMoves[i].dir == 'L') {
-      for (let j = 0; j < aMoves[i].count; j++) {
-
-        let prevHead = {x: headPos.x, y: headPos.y };
-
-        headPos.x--;
-
-        if (!IsNear(headPos, tailPos))
-          UpdateKnotPos(tailPos, prevHead, tailPath);
-      }
-    }
-    else if (aMoves[i].dir == 'U') {
-      for (let j = 0; j < aMoves[i].count; j++) {
-
-        let prevHead = {x: headPos.x, y: headPos.y };
-
-        headPos.y++;
-
-        if (!IsNear(headPos, tailPos))
-          UpdateKnotPos(tailPos, prevHead, tailPath);
-      }
-    }
-    else if (aMoves[i].dir == 'D') {
-      for (let j = 0; j < aMoves[i].count; j++) {
-
-        let prevHead = {x: headPos.x, y: headPos.y };
-
-        headPos.y--;
-
-        if (!IsNear(headPos, tailPos))
-          UpdateKnotPos(tailPos, prevHead, tailPath);
-      }
-    }
+    MoveKnots(knots, aMoves[i].dir, aMoves[i].count, tailPath);
+    PrintKnots(knots);
   }
 
   return tailPath;
 }
 
-let bb = util.MapInput('./Day9Input.txt', (aElem) => {
+let bb = util.MapInput('./Day9TestInput2.txt', (aElem) => {
   let dd = aElem.split(' ');
   return { dir: dd[0], count: parseInt(dd[1]) };
 }, '\r\n');
 
 console.log(bb);
 
-let tailPath = ComputePath(bb);
+let tailPath = ComputePath(bb, 10);
 
 console.log(util.ComputeMapSize(tailPath));
