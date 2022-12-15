b2abb110b5dfc2b4feef3a6ba019f017a1b2aa6f
Costi Rada <costi.rada@caphyon.com>
2022-12-15 08:41:27 +0200

    Day 9 part 2 solution

--- Day9.js
+++ Day9.js
@@ -5,15 +5,17 @@ function ComputeKey(aPos) {
   return aPos.x + "#" + aPos.y;
 }
 
-function IsNear(aPos1, aPos2) {
-  let dist = Math.abs(aPos2.x - aPos1.x) + Math.abs(aPos2.y - aPos1.y);
-  return ((aPos1.x == aPos2.x) || (aPos1.y == aPos2.y))  ? (dist <= 1) : (dist <= 2);
+function ComputeDist(aPos1, aPos2) {
+  return Math.abs(aPos2.x - aPos1.x) + Math.abs(aPos2.y - aPos1.y);
 }
 
-function UpdateKnotPos(aKnot, aIsTail, aNewPos, aDir, aPath) {
-  
-  let oo = (aKnot.x != aNewPos.x) && (aKnot.y != aNewPos.y);
-  UpdateKnot(aKnot, aDir, oo, aNewPos);
+function IsNear(aPos1, aPos2) {
+  let dist = ComputeDist(aPos1, aPos2);
+  return ((aPos1.x == aPos2.x) || (aPos1.y == aPos2.y)) ? (dist <= 1) : (dist <= 2);
+}
+
+function UpdateKnotPos(aKnot, aHead, aIsTail, aDir, aPath) {
+  UpdateKnot(aKnot, aHead, aDir);
 
   if (aIsTail) {
     let key = ComputeKey(aKnot);
@@ -29,56 +31,88 @@ function CopyNode(aNode1, aNode2) {
   aNode1.y = aNode2.y;
 }
 
-function UpdateKnot(aKnot, aDir, aMoveDiag, aHead) {
-  if (aDir == 'R') {
-    aKnot.x++;
-    if (aMoveDiag)
-      (aKnot.y < aHead.y) ? aKnot.y-- : aKnot.y++;
-  }
-  else if (aDir == 'L') {
-    aKnot.x--;
-    if (aMoveDiag)
-      (aKnot.y < aHead.y) ? aKnot.y++ : aKnot.y--;
-  }
-  else if (aDir == 'U') {
-    aKnot.y++;
-    if (aMoveDiag)
-      (aKnot.x < aHead.x) ? aKnot.x++ : aKnot.x--;
+function UpdateKnot(aKnot, aHead, aDir) {
+
+  if (aHead) {
+
+    let moveDiag = (aKnot.x != aHead.x && aKnot.y != aHead.y);
+    if (moveDiag) {
+    const tt = [[1, 1], [1, -1], [-1, 1], [-1, -1]];
+  
+    let hh = 1;
+    for (;;) { 
+    for (let i = 0; i < tt.length; i++) {
+      let x = aKnot.x + tt[i][0];
+      let y = aKnot.y + tt[i][1];
+
+      let dist = ComputeDist({ x: x, y: y }, aHead);
+
+      if (dist == hh) {
+        aKnot.x = x;
+        aKnot.y = y;
+
+        return;
+      }
+    }
+    hh++;
+    }
   }
   else {
-    aKnot.y--;
-    if (aMoveDiag)
-      (aKnot.x < aHead.x) ? aKnot.x-- : aKnot.x++;
+    const tt = [[0, 1], [0, -1], [1, 0], [-1, 0]];
+
+    for (let i = 0; i < tt.length; i++) {
+      let x = aKnot.x + tt[i][0];
+      let y = aKnot.y + tt[i][1];
+
+      let dist = ComputeDist({ x: x, y: y }, aHead);
+
+      if (dist == 1) {
+        aKnot.x = x;
+        aKnot.y = y;
+
+        return;
+      }
+    }
+  }
+  }
+  else {
+    if (aDir == 'R')
+      aKnot.x++;
+    else if (aDir == 'L')
+      aKnot.x--;
+    else if (aDir == 'U')
+      aKnot.y++;
+    else
+      aKnot.y--;
   }
 }
 
 function MoveKnots(aKnots, aDir, aCount, aTailPath) {
   for (let j = 0; j < aCount; j++) {
 
-    if (aDir == 'L' && aCount == 8)
-      PrintKnots(aKnots);
+   // if (aDir == 'R' && aCount == 17)
+    //  PrintKnots(aKnots);
 
-    UpdateKnot(aKnots[0], aDir, false, null);
+    UpdateKnot(aKnots[0], null, aDir);
 
     for (let k = 1; k < aKnots.length; k++) {
       if (!IsNear(aKnots[k], aKnots[k - 1])) {
-        UpdateKnotPos(aKnots[k], (k == (aKnots.length - 1)), aKnots[k - 1], aDir, aTailPath);
+        UpdateKnotPos(aKnots[k], aKnots[k - 1], (k == (aKnots.length - 1)), aDir, aTailPath);
       }
     }
-  }  
+  }
 }
 
 function PrintKnots(aKnots) {
-   let grid = new mm.Matrix(30, 30, '.');
-   
-   let x = 10;
-   let y = 10;
-   for (let i = 0; i < aKnots.length; i++)
-   {
-     grid.SetValue(aKnots[i].y + y, aKnots[i].x + x, i == 0 ? 'H' : i.toString());
-   }
+  let grid = new mm.Matrix(50, 50, '.');
 
-   grid.PrintReverse('');
+  let x = 10;
+  let y = 10;
+  for (let i = aKnots.length - 1; i >= 0 ; i--) {
+    grid.SetValue(aKnots[i].y + y, aKnots[i].x + x, i == 0 ? 'H' : i.toString());
+  }
+
+  grid.PrintReverse('');
 }
 
 function ComputePath(aMoves, aKnotCount) {
@@ -94,13 +128,13 @@ function ComputePath(aMoves, aKnotCount) {
     console.log("\n == " + aMoves[i].dir + " " + aMoves[i].count + " == \n");
 
     MoveKnots(knots, aMoves[i].dir, aMoves[i].count, tailPath);
-    PrintKnots(knots);
+    //PrintKnots(knots);
   }
 
   return tailPath;
 }
 
-let bb = util.MapInput('./Day9TestInput2.txt', (aElem) => {
+let bb = util.MapInput('./Day9Input.txt', (aElem) => {
   let dd = aElem.split(' ');
   return { dir: dd[0], count: parseInt(dd[1]) };
 }, '\r\n');
