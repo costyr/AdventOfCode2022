2ade0222b9ee904c581d1b4988b59d5449da5165
Costi Rada <costi.rada@caphyon.com>
2023-01-23 15:02:04 +0200

    Code refactoring

--- Day20.js
+++ Day20.js
@@ -1,96 +1,39 @@
 const util = require('./Util.js');
 const ll = require('./LinkedList.js');
 
-function Transform(aNumbers) {
-
-  let nn = util.CopyObject(aNumbers);
-  for (let i = 0; i < aNumbers.length; i++) {
-    let hh = aNumbers[i];
-
-    let index = nn.indexOf(hh);
-
-    let ii = (index + hh + (hh >= 0 ? 1 : 0)) % aNumbers.length;
-    if (ii < 0)
-      ii = aNumbers.length + ii;
-    else if (ii == 0)
-      ii = aNumbers.length;
-
-    if (ii == index)
-      continue;
-
-    nn.splice(ii, 0, hh);
-
-    if (ii < index)
-      index ++;
-    nn.splice(index, 1);
-
-    //console.log(JSON.stringify(nn));
-  }
-
-  for (let i = 0; i < aNumbers.length; i++)
-   if (nn.indexOf(aNumbers[i]) == -1)
-     console.log("Not found: " + aNumbers[i]);
-
-  let yy = nn.indexOf(0);
-
-  let d1 = nn[(yy + 1000) % aNumbers.length];
-  let d2 = nn[(yy + 2000) % aNumbers.length];
-  let d3 = nn[(yy + 3000) % aNumbers.length];
-
-  console.log(d1 + " " + d2 + " " + d3);
-
-  return d1 + d2 + d3;
-}
-
-function Transform2(aNumbers) {
+function Transform(aNumbers, aMixCount, aFactor) {
   let list = new ll.LinkedList();
-  
+
   let nodes = [];
   for (let i = 0; i < aNumbers.length; i++)
-    nodes.push(list.AddTail(aNumbers[i]));
+    nodes.push(list.AddTail(aNumbers[i] * aFactor));
 
-  for (let k = 0; k < 10; k++) {
-  for (let i = 0; i < nodes.length; i++)
-  {
-    let node = nodes[i];
+  for (let k = 0; k < aMixCount; k++) {
+    for (let i = 0; i < nodes.length; i++) {
+      let node = nodes[i];
 
-    if (node.mValue > 0) {
+      if (node.mValue > 0) {
 
-      let n = node.mNext;
+        let n = node.mNext;
         if (n == null)
           n = list.mHead;
-      list.RemoveNode(node);
-      let nth = list.GetNodeNthPosFromNode(n, (node.mValue - 1) % list.mSize);
+        list.RemoveNode(node);
+        let nth = list.GetNodeNthPosFromNode(n, (node.mValue - 1) % list.mSize);
 
-      //if (nth != node) {
         list.AddNodeAfter(nth, node);
-      //}
-      //else
-      //{
-      //  console.log(nth.mValue + " " + node.mValue);
-     // }
-    } 
-    else if (node.mValue < 0)
-    {
-      let n = node.mPrev;
+
+      }
+      else if (node.mValue < 0) {
+        let n = node.mPrev;
         if (n == null)
           n = list.mTail;
-      list.RemoveNode(node);
-      let nth = list.GetNodeNthPosBackFromNode(n, Math.abs(node.mValue) % list.mSize);
-      
-      //if (nth != node) {
-        list.AddNodeAfter(nth, node);
-      //}
-      //else
-     // {
-       // console.log(nth.mValue + " " + node.mValue);
-     // }
-    }      
+        list.RemoveNode(node);
+        let nth = list.GetNodeNthPosBackFromNode(n, Math.abs(node.mValue) % list.mSize);
 
-    //console.log(node.mValue + " => " + list.ToString());
+        list.AddNodeAfter(nth, node);
+      }
+    }
   }
-  console.log(k);
-}
 
   let node = list.GetNodeWithValue(0);
 
@@ -98,17 +41,13 @@ function Transform2(aNumbers) {
   let d2 = list.GetNodeNthPosFromNode(node, 2000).mValue;
   let d3 = list.GetNodeNthPosFromNode(node, 3000).mValue;
 
-  console.log(d1 + " " + d2 + " " + d3);
-
   return d1 + d2 + d3;
 }
 
 let numbers = util.MapInput('./Day20Input.txt', (aElem) => {
-  return parseInt(aElem) * 811589153;
-  }, '\r\n');
+  return parseInt(aElem);
+}, '\r\n');
 
-//console.log(numbers);
+console.log(Transform(numbers, 1, 1));
 
-//console.log(Transform(numbers));
-
-console.log(Transform2(numbers));
+console.log(Transform(numbers, 10, 811589153));
